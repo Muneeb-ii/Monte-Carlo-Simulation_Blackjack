dce35ccebcc1e37955933f135a6bf19feb7bef79
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-19 14:29:47 -0500

    Shuffle Class Implemented

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -3,5 +3,26 @@ import java.util.Random;
 public class Shuffle {
     public static void main (String[] args) {
 
+        ArrayList<Integer> arr0 = new ArrayList<>();
+        Random randomizer = new Random();
+
+        for(int i=0; i<10; i++){
+            arr0.add(randomizer.nextInt(100));
+            System.out.println(arr0.get(i));
+        }
+        for(int i=0; i<10; i++){
+            System.out.println(arr0.get(i));
+        }
+
+        for(int i=0; i<10; i++){
+            int indexToRemove = randomizer.nextInt(arr0.size());
+            System.out.print(arr0.get(indexToRemove) + " ");
+            arr0.remove(indexToRemove);
+            
+            for(int j=0; j<arr0.size(); j++){
+                System.out.print(arr0.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
