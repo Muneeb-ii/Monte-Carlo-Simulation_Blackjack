83857d8cfeea9538f40ac17362fd7fe02f421e7f
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-21 21:44:33 -0500

    Create Hand.java

---
+++ src/Hand.java
@@ -0,0 +1,50 @@
+public class Hand {
+
+    /**
+     * Creates an empty hand as an ArrayList of Cards.  
+     */
+    public Hand(){
+    }
+
+    /**
+     * Removes any cards currently in the hand. 
+     */
+    public void reset(){
+    }
+
+    /**
+     * Adds the specified card to the hand.
+     * @param card the card to be added to the hand
+     */
+    public void add(Card card){
+    }
+
+    /**
+     * Returns the number of cards in the hand.
+     * @return the number of cards in the hand
+     */
+    public int size(){
+    }
+
+    /**
+     * Returns the card in the hand specified by the given index. 
+     * @param index the index of the card in the hand.
+     * @return the card in the hand at the specified index.
+     */
+    public Card getCard(int index){
+    }
+
+    /**
+     * Returns the summed value over all cards in the hand.
+     * @return the summed value over all cards in the hand
+     */
+    public int getTotalValue(){
+    }
+
+    /**
+     * Returns a string representation of the hand.
+     * @return a string representation of the hand
+     */
+    public String toString(){
+    }
+}
