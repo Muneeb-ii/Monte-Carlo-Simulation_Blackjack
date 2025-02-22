018ebc8a049a66d20acd6cd331a6fbf3c1defc90
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-21 21:33:31 -0500

    Create Card.java

---
+++ src/Card.java
@@ -0,0 +1,31 @@
+public class Card {
+
+    /**
+     * The value of the card.
+     */
+    private int value;
+
+    /**
+     * Constructs a card with the specified value.
+     * @param val
+     */
+    public Card(int val) {
+        // TBD
+    }
+
+    /**
+     * Returns the value of the card.
+     * @return the value of the card
+     */
+    public int getValue() {
+        // TBD
+    }
+    
+    /**
+     * Returns a string representation of this card.
+     * @return a string representation of this card
+     */
+    public String toString() {
+        // TBD
+    }
+}
