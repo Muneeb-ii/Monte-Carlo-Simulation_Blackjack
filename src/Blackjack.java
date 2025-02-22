4918edea4c6636ace6fd555d1e6f46b9ab8d8473
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-22 03:19:23 -0500

    Added file header and constructor

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,3 +1,21 @@
+/**
+* Author: Muneeb Nafees
+* 
+* Purpose of the class: A basic implementation of a complete Blackjack game.
+*/
+
 public class Blackjack {
-    
+    private Deck deck;
+    private Hand playerHand;
+    private Hand dealerHand;
+
+    /*
+     * Constructs a new Blackjack game with a new deck, player hand, and dealer hand.
+     */
+    public Blackjack() {
+        deck = new Deck();
+        playerHand = new Hand();
+        dealerHand = new Hand();
+        deck.shuffle();
+    }
 }
