126d85ef6608a0cd7f65768eb73b0cc5fdc0d2e5
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-21 22:32:11 -0500

    Create Deck.java

---
+++ src/Deck.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class Deck {
+
+    /**
+     * Creates the underlying deck as an ArrayList of Card objects. 
+     * Calls build() as a subroutine to build the deck itself.
+     */
+    public Deck() {
+    }
+
+    /**
+     * Builds the underlying deck as a standard 52 card deck. 
+     * Replaces any current deck stored. 
+     */
+    public void build() {
+    }
+
+    /**
+     * Returns the number of cards left in the deck. 
+     * @return the number of cards left in the deck
+     */
+    public int size() {
+    }
+
+    /**
+     * Returns and removes the first card of the deck.
+     * @return the first card of the deck
+     */
+    public Card deal() {
+    }
+
+    /**
+     * Shuffles the cards currently in the deck. 
+     */
+    public void shuffle() {
+    }
+
+    /**
+     * Returns a string representation of the deck.
+     * @return a string representation of the deck
+     */
+    public String toString() {
+    }
+}
