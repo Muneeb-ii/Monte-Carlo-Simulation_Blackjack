b124245e8d63775905c07401ebc66123edfbd289
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-22 03:11:17 -0500

    Create BlackjackTests.java

---
+++ src/BlackjackTests.java
@@ -0,0 +1,30 @@
+/*
+file name:      BlackjackTests.java
+Authors:        Max Bender & Naser Al Madi
+last modified:  8/28/2022
+
+How to run:     java -ea BlackjackTests
+*/
+
+//Expected results:
+//Draw percent: 8 +/- 1
+//Dealer win percent: 49 +/- 1
+//Player win percent: 41 +/1 1
+
+
+public class BlackjackTests {
+
+    public static void blackjackTests() {
+
+        //TODO
+        //Simulate 10,000 blackjack games and compute the 
+        //percents of draws, dealer wins and player wins
+        //Your results should match the expected results above.
+
+    }
+
+    public static void main(String[] args) {
+
+        blackjackTests();
+    }
+}
