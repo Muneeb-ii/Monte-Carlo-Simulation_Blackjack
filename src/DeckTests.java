a9ec41f4ec0a95864277916afc438049142eda5b
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-02-26 19:20:51 -0500

    Added two test cases
    Test case 5: To test the correct number of each card
    
    Test case 6: To test the randomness of each reshuffle

--- src/DeckTests.java
+++ src/DeckTests.java
@@ -76,9 +76,55 @@ public class DeckTests {
             assert d1.size() == 52 : "Error in Deck::shuffle()";
         }
 
-        // todo: add case to check correct number of each card
-        // todo: add case to check reshuffle randomness
+        // case 5: testing correct number of each card
+        {
+            // set up
+            Deck d1 = new Deck();
 
+            // verify
+
+
+             // test
+            int[] cardCount = new int[11];
+            for (int i = 0; i < 11; i++) {
+                cardCount[i] = 0;
+            }
+           
+            for (int i = 0; i < 52; i++) {
+                Card c = d1.deal();
+                cardCount[c.getValue()-1]++;
+            }
+
+            for (int i = 1; i < 8; i++) {
+                assert cardCount[i] == 4 : "Error in Deck::Deck()"; // 4 2s - 8s
+            }
+            
+            assert cardCount[0] == 0 : "Error in Deck::Deck()"; // 0 1s
+            assert cardCount[9] == 16 : "Error in Deck::Deck()"; // 16 10s
+            assert cardCount[10] == 4 : "Error in Deck::Deck()"; // 4 11s
+        }
+    
+        
+        // case 6: testing reshuffle randomness
+        {
+            // set up
+            Deck d1 = new Deck();
+
+            // verify
+            
+
+            // test
+            String before = d1.toString();
+            d1.shuffle();
+            String after = d1.toString();
+            d1.shuffle();
+            String after2 = d1.toString();
+
+            assert !before.equals(after) : "Error in Deck::shuffle()"; // check that shuffle is random
+            assert !before.equals(after2) : "Error in Deck::shuffle()"; // check that reshuffle is random
+            assert !after.equals(after2) : "Error in Deck::shuffle()"; // check that reshuffle is random
+            assert d1.size() == 52 : "Error in Deck::shuffle()"; // check that deck size is maintained
+        }
         System.out.println("*** Done testing Deck! ***\n");
     }
 
